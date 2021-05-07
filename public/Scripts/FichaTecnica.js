2af1652d46b4ab611f2c91a626bfeaa837253b6f
Adria Vieira <adriavieira731@gmail.com>
2021-05-07 14:47:14 -0400

    mostrar lista de fichas tecnicas dinamicamente

--- public/Scripts/FichaTecnica.js
+++ public/Scripts/FichaTecnica.js
@@ -1,4 +1,4 @@
-$("#toshiba").hide()    
+﻿$("#toshiba").hide()    
 $("#pastore").hide()
 $("#pastoreList").hide()
 $("#toshibaList").hide()
@@ -8,8 +8,10 @@ $('#maquinas').change(e => {
    var split = e.target.value.split(",");
    var v1 = split[0];
    var v2 = split[1];
+   var splitName = v2.split(" ");
+   var v3 = splitName[0]
 
-   if (v2 === 'HAITIAN') {
+   if (v2 === 'HAITIAN' || v3 === "Haitian") {
 
       $("#maquinaHaitian").val(v1)
       $("#pastore").show()
@@ -29,17 +31,20 @@ $('#maquinasList').change(e => {
    var split = e.target.value.split(",");
    var v1 = split[0];
    var v2 = split[1];
+   var v2 = split[1];
+   var splitName = v2.split(" ");
+   var v3 = splitName[0]
 
    $.ajax({
       url: "/maquinaById/"+ v1,
       type: "get", //send it through get method
       success: function(responseMaquina) {
-
+         console.log(responseMaquina)
          $.ajax({
             url: "/ficha/lista/"+ v1 + "/" + v2,
             type: "get", //send it through get method
             success: function(response) {
-               if (v2 === 'HAITIAN') {
+               if (v2 === 'HAITIAN' || v3 === "Haitian") {
                   addRow(response.length, response, 'Haitian', responseMaquina.descricao)
                } else if (v2 === 'Toshiba') {
                   addRow(response.length, response, 'Toshiba', responseMaquina.descricao)
@@ -78,24 +83,3 @@ function addRow(rows, data, path, maquina) {
    $('#tableBody').html(tbody);
    
 }
-
-// function addRow(rows, data, path) {
-//    var tbody = '';
-//    for (var i = 0; i < rows; i++) {
-//       tbody +=  "<tr> <td>" + data[i].id +" </td> \
-//                   <td>" + data[i].maq +"</td> \
-//                   <td>" + data[i].maquina.descricao +"</td> \
-//                   <td>\
-//                   <form method='GET' action='/fichas/edit" + path  + "/" + data[i].id  + "' style='display: inline;'><button class='btn btn-warning'> Editar</button></form> \
-//                   <form method='POST' action='/fichas/delete" + path + "'" +  "style='display: inline;' onsubmit='confirmarDelecao(event, this)'> \
-//                      <input type='hidden' name='id' value='" + data[i].id  + "'> \
-//                      <button class='btn btn-danger'> Remover</button> \
-//                   </form> \
-//                   <form method='GET' action='/ficha/revisao" + path  + "/" + data[i].id  + "' style='display: inline;'><button class='btn btn-info'> Revisão</button></form></td> \
-//                </tr>";
-//    }
-
-//    $('#tableBody').html(tbody);
-   
-// }
-
